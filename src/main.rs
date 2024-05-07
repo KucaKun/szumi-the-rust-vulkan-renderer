19edd8a873a51fa78f6265b1a031e9dc57001fec
Adrian Kuca <adriankuca2@gmail.com>
2024-05-07 18:44:25 +0200

    Add first window in vulkanalia, but will switch to vulkano

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,95 @@
-fn main() {
-    println!("Hello, world!");
+#![allow(
+    dead_code,
+    unused_variables,
+    clippy::too_many_arguments,
+    clippy::unnecessary_wraps
+)]
+
+use anyhow::{anyhow, Result};
+use log::*;
+use vulkanalia::loader::{LibloadingLoader, LIBRARY};
+use vulkanalia::prelude::v1_0::*;
+use vulkanalia::window as vk_window;
+use winit::dpi::LogicalSize;
+use winit::event::{Event, WindowEvent};
+use winit::event_loop::EventLoop;
+use winit::window::{Window, WindowBuilder};
+
+unsafe fn create_instance(window: &Window, entry: &Entry) -> Result<Instance> {
+    let application_info = vk::ApplicationInfo::builder()
+        .application_name(b"Vulkan Tutorial\0")
+        .application_version(vk::make_version(1, 0, 0))
+        .engine_name(b"No Engine\0")
+        .engine_version(vk::make_version(1, 0, 0))
+        .api_version(vk::make_version(1, 0, 0));
+
+    let extensions = vk_window::get_required_instance_extensions(window)
+        .iter()
+        .map(|e| e.as_ptr())
+        .collect::<Vec<_>>();
+
+    let info = vk::InstanceCreateInfo::builder()
+        .application_info(&application_info)
+        .enabled_extension_names(&extensions);
+
+    Ok(entry.create_instance(&info, None)?)
 }
+
+fn main() -> Result<()> {
+    pretty_env_logger::init();
+
+    // Window
+    let event_loop = EventLoop::new()?;
+    let window = WindowBuilder::new()
+        .with_title("Vulkan Tutorial (Rust)")
+        .with_inner_size(LogicalSize::new(1024, 768))
+        .build(&event_loop)?;
+    // App
+    let mut app = unsafe { App::create(&window)? };
+    event_loop.run(move |event, elwt| {
+        match event {
+            // Request a redraw when all events were processed.
+            Event::AboutToWait => window.request_redraw(),
+            Event::WindowEvent { event, .. } => match event {
+                // Render a frame if our Vulkan app is not being destroyed.
+                WindowEvent::RedrawRequested if !elwt.exiting() => {
+                    unsafe { app.render(&window) }.unwrap()
+                }
+                // Destroy our Vulkan app.
+                WindowEvent::CloseRequested => {
+                    elwt.exit();
+                    unsafe {
+                        app.destroy();
+                    }
+                }
+                _ => {}
+            },
+            _ => {}
+        }
+    })?;
+
+    Ok(())
+}
+
+/// Our Vulkan app.
+#[derive(Clone, Debug)]
+struct App {}
+
+impl App {
+    /// Creates our Vulkan app.
+    unsafe fn create(window: &Window) -> Result<Self> {
+        Ok(Self {})
+    }
+
+    /// Renders a frame for our Vulkan app.
+    unsafe fn render(&mut self, window: &Window) -> Result<()> {
+        Ok(())
+    }
+
+    /// Destroys our Vulkan app.
+    unsafe fn destroy(&mut self) {}
+}
+
+/// The Vulkan handles and associated properties used by our Vulkan app.
+#[derive(Clone, Debug, Default)]
+struct AppData {}
