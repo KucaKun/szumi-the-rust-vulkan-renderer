65caf310bd6ecc8ab55a360ee3ea3cd7a817167f
Adrian Kuca <adriankuca2@gmail.com>
2024-05-13 15:08:03 +0200

    Yellow triangle example

--- src/vertex.rs
+++
@@ -1,9 +0,0 @@
-use vulkano::buffer::BufferContents;
-use vulkano::pipeline::graphics::vertex_input::Vertex;
-
-#[derive(BufferContents, Vertex)]
-#[repr(C)]
-struct MyVertex {
-    #[format(R32G32_SFLOAT)]
-    position: [f32; 2],
-}
